e111c58cc96aa18654f5bdea411a2d7dba280f27
Huynt0811 <92984865+Huynt0811@users.noreply.github.com>
2023-02-10 00:18:23 +0700

    13. Hoàn thiện CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -4,12 +4,14 @@ let getHomepage = async (req, res) => {
   const [row, fields] = await pool.execute("SELECT * FROM users");
   return res.render("index.ejs", { dataUser: row });
 };
+// Lấy danh sách người dùng User
 let getDetailPage = async (req, res) => {
   let userId = req.params.userId;
   let [user] = await pool.execute("SELECT * FROM users WHERE id = ?", [userId]);
   console.log(user[0]);
   return res.send("Helo detail");
 };
+// Thêm mới người dùng User
 let createNewUser = async (req, res) => {
   let { fisrtName, lastName, email, address } = req.body;
 
@@ -20,9 +22,31 @@ let createNewUser = async (req, res) => {
 
   return res.redirect("/");
 };
-
+// Xóa người dùng User
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute("delete from users where id = ?", [userId]);
+  return res.redirect("/");
+};
+// Edit người dùng User
+let getEditPage = async (req, res) => {
+  let userId = req.params.id;
+  let [user] = await pool.execute("select * from users where id = ?", [userId]);
+  return res.render("update.ejs", { dataUser: user[0] });
+};
+let postUpdateUser = async (req, res) => {
+  let { fisrtName, lastName, email, address, id } = req.body;
+  await pool.execute(
+    "UPDATE users SET fisrtName = ?, lastName = ?, email= ?,address = ? WHERE id = ?",
+    [lastName, fisrtName, email, address, id]
+  );
+  return res.redirect("/");
+};
 module.exports = {
   getHomepage,
   getDetailPage,
   createNewUser,
+  deleteUser,
+  getEditPage,
+  postUpdateUser,
 };
